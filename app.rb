39a1717756a11be41a520bda37b6c802da290c49
Kiara Winslow <kiarawinslow@gmail.com>
2018-06-26 18:05:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "<h1>My name is Kiara.</h1>"
+  end
+  get '/hometown' do 
+    "<h1>My hometown is Oaktown.</h1>"
+  end
+  get '/favorite-song' do 
+    "<h1>My favorite song is Passionfriuit .</h1>"
+  end
 end
