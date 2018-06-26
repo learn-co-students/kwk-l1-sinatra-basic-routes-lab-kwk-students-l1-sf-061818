09c02f922b65b0467332d6d0f79436485b91ad64
papayajaia <jpaklossy@gmail.com>
2018-06-26 23:20:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require './config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Jaia."
+  end 
+  
+get '/hometown' do 
+    "My hometown is Quebec, Canada."
+  end 
+  
+get '/favorite-song' do 
+    "My favorite song is - I dont have one, I love all music!!!"
+  end 
+  
 end
